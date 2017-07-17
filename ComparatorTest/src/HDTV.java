f7cef4d29fe37ac49d24774bf8ca6cc593780cdd
SauravYadav12 <saurav1286@gmail.com>
2017-07-17 10:10:36 -0400

    Update HDTV.java

--- ComparatorTest/src/HDTV.java
+++ ComparatorTest/src/HDTV.java
@@ -4,7 +4,7 @@ public class HDTV {
 	String Brandname;
 	int Size;
 	
-	public HDTV(String Brandname,int Size)
+	public HDTV(String Brandname,int Size) // Creating a parameterized constructor.
 	{
 		this.Brandname = Brandname;
 		this.Size = Size;
